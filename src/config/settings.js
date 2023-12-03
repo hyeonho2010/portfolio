bc349809fa13f5650dd293b2fdf66af91083885d
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-03 20:47:47 +0900

    Version 1.3.2

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `hyeonho64`,
+	description: `hyeonho64의 포트폴리오 사이트입니다.`,
+	url: `https://hyeonho64.vercel.app`,
+	name: `hyeonho64`,
   enableThemeSwitcher: true,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+  showLittleSticksPlug: false,
 };
