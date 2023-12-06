7d75c502816fd62c0daa7796e0927571f9543418
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-06 22:25:07 +0900

    Version 1.3.4

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `hyeonho64`,
-	description: `hyeonho64의 포트폴리오 사이트입니다.`,
-	url: `https://hyeonho64.vercel.app`,
-	name: `hyeonho64`,
+	title: `hyeonho2010`,
+	description: `hyeonho2010의 포트폴리오 사이트입니다.`,
+	url: `https://hyeonho2010.vercel.app`,
+	name: `hyeonho2010`,
   enableThemeSwitcher: true,
   showLittleSticksPlug: false,
 };
